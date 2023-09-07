b42acb5699ae2bbcf09cc13569cec689b2f4c1af
Anubhav Chaudhary <31804245+123anubhav@users.noreply.github.com>
2023-09-07 13:57:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-let qrContentInput = document.getElementById("qr-content");
-let qrGenerationForm =
-    document.getElementById("qr-generation-form");
-let qrCode;
+function copyToClipboard(text) {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
 
-function generateQrCode(qrContent) {
-    return new QRCode("qr-code", {
-        text: qrContent,
-        width: 256,
-        height: 256,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.H,
-    });
+    const copySuccess = document.getElementById('copySuccess');
+    copySuccess.style.opacity = '1';
+    setTimeout(() => {
+        copySuccess.style.opacity = '0';
+    }, 1500);
 }
-
-qrGenerationForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    let qrContent = qrContentInput.value;
-    if (qrCode == null) {
-        qrCode = generateQrCode(qrContent);
-    } else {
-        qrCode.makeCode(qrContent);
-    }
-
-    // Create a download link for the QR code
-    const qrCanvas = document.querySelector("#qr-code canvas");
-    const downloadLink = document.getElementById("download-link");
-    downloadLink.style.display = "block";
-    downloadLink.href = qrCanvas.toDataURL("image/png");
-});
+copyToClipboard(text);
